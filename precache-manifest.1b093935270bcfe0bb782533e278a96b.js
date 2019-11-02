dce09b7ab0a7f7df9fb6ff59baf1812d82ddb4be
Tiarê Balbi Bonamini <me@tiarebalbi.com>
2019-11-02 19:15:28 +0000

    Updates

---
+++ precache-manifest.1b093935270bcfe0bb782533e278a96b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd99b0924c6acc9fa9e74083271d6e9",
+    "url": "/flux-main-sample/index.html"
+  },
+  {
+    "revision": "5addf432b663e5605df7",
+    "url": "/flux-main-sample/static/js/2.0afb8714.chunk.js"
+  },
+  {
+    "revision": "c9ed4033bd29540a942e",
+    "url": "/flux-main-sample/static/js/3.bf449d08.chunk.js"
+  },
+  {
+    "revision": "6a56722dd304d3ae9a4e",
+    "url": "/flux-main-sample/static/js/main.b869a175.chunk.js"
+  },
+  {
+    "revision": "92cd474533a3dc574d4f",
+    "url": "/flux-main-sample/static/js/runtime-main.fef4b28e.js"
+  }
+]);
